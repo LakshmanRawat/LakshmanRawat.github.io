f823461835730a43b8b27e01f614c7608877307f
Laxman Singh <48987933+LakshmanRawat@users.noreply.github.com>
2024-12-12 08:10:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Example: Scroll to sections smoothly
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', event => {
+    event.preventDefault();
+    document.querySelector(event.target.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
